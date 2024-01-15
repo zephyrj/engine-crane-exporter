e3d69f71cc5ea1d235d07d7074f59ba336e6ff1d
zephyrj <zephyrj@protonmail.com>
2024-01-15 19:39:43 +0000

    Tidy-up ui handling

--- EngineCraneExporter/UIParameters.cpp
+++ EngineCraneExporter/UIParameters.cpp
@@ -3,38 +3,41 @@
 
 namespace ui {
 	// TODO: Try FamilyName/VariantName
-	const std::array<std::pair<std::wstring, std::wstring>, NUM_STRINGS> STRING_DATA = {
-		std::make_pair(L"File Name", L"[PlayerName] [PlatformName] [TrimName]")
+	static const std::array<StringElementType, NUM_STRINGS> STRING_ELEMENTS = {
+		StringElementType(L"Filename", L"", L"[PlayerName][PlatformName][TrimName]"),
+		StringElementType(L"Exporter Script Path", L"", L""),
 	};
-	const std::array<std::pair<std::wstring, bool>, NUM_BOOLS> BOOL_DATA;
+
+	static const std::array<BoolElementType, NUM_BOOLS> BOOL_ELEMENTS = {
+	};
 
 	AuCarExpErrorCode SetupStringData(AuCarExpArray<AuCarExpUIStringData>& stringData)
 	{
-		if (stringData.GetCount() != STRING_DATA.size())
-			return AuCarExpErrorCode_UnknownError;
-
-		int idx = 0;
-		for (auto& pair : STRING_DATA)
-		{
-			wcscpy_s(stringData[idx].Label, pair.first.c_str());
-			wcscpy_s(stringData[idx].Value, pair.second.c_str());
-			idx += 1;
-		}
-		return AuCarExpErrorCode_Success;
+		return ui::SetupUIData(STRING_ELEMENTS, stringData);
 	}
 
 	AuCarExpErrorCode SetupBoolData(AuCarExpArray<AuCarExpUIBoolData>& boolData)
 	{
-		if (boolData.GetCount() != BOOL_DATA.size())
-			return AuCarExpErrorCode_UnknownError;
+		return ui::SetupUIData(BOOL_ELEMENTS, boolData);
+	}
 
-		int idx = 0;
-		for (auto& pair : BOOL_DATA)
-		{
-			wcscpy_s(boolData[idx].Label, pair.first.c_str());
-			boolData[0].Value = pair.second;
-			idx += 1;
-		}
-		return AuCarExpErrorCode_Success;
+	StringElementType::StringElementType() = default;
+	StringElementType::StringElementType(const wchar_t* label, const wchar_t* tooltip, const wchar_t* default_value)
+		: label(label), tooltip(tooltip), default_value(default_value) {}
+	void StringElementType::update(StringElementType::AUDataType& stringData) const
+	{
+		wcscpy_s(stringData.Label, label);
+		wcscpy_s(stringData.ToolTip, tooltip);
+		wcscpy_s(stringData.Value, default_value);
+	}
+
+	BoolElementType::BoolElementType() = default;
+	BoolElementType::BoolElementType(const wchar_t* label, const wchar_t* tooltip, bool default_value)
+		: label(label), tooltip(tooltip), default_value(default_value) {}
+	void BoolElementType::update(BoolElementType::AUDataType& boolData) const
+	{
+		wcscpy_s(boolData.Label, label);
+		wcscpy_s(boolData.ToolTip, tooltip);
+		boolData.Value = default_value;
 	}
 }
--- EngineCraneExporter/UIParameters.h
+++ EngineCraneExporter/UIParameters.h
@@ -1,15 +1,57 @@
-#pragma once
-
-#include "pch.h"
 #include "AuCarExport.h"
 
 namespace ui {
-	constexpr unsigned int NUM_STRINGS = 1;
-	extern const std::array<std::pair<std::wstring, std::wstring>, NUM_STRINGS> STRING_DATA;
+	enum class StringElement : size_t {
+		Filename = 0,
+		ExporterScriptPath,
+		Count
+	};
 
-	constexpr unsigned int NUM_BOOLS = 0;
-	extern const std::array<std::pair<std::wstring, bool>, NUM_BOOLS> BOOL_DATA;
+	enum class BoolElement : size_t {
+		Count = 0,
+	};
+
+	constexpr size_t NUM_STRINGS = static_cast<size_t>(StringElement::Count);
+	constexpr size_t NUM_BOOLS = static_cast<size_t>(BoolElement::Count);
 
 	AuCarExpErrorCode SetupStringData(AuCarExpArray<AuCarExpUIStringData>& stringData);
 	AuCarExpErrorCode SetupBoolData(AuCarExpArray<AuCarExpUIBoolData>& boolData);
+
+	class StringElementType {
+	public:
+		using AUDataType = AuCarExpUIStringData;
+		StringElementType();
+		StringElementType(const wchar_t* label, const wchar_t* tooltip, const wchar_t* default_value);
+		void update(AUDataType& data) const;
+		const wchar_t* label = nullptr;
+		const wchar_t* tooltip = nullptr;
+		const wchar_t* default_value = nullptr;
+	};
+
+	class BoolElementType {
+	public:
+		using AUDataType = AuCarExpUIBoolData;
+		BoolElementType();
+		BoolElementType(const wchar_t* label, const wchar_t* tooltip, bool default_value);
+		void update(AUDataType& data) const;
+		const wchar_t* label = nullptr;
+		const wchar_t* tooltip ;
+		bool default_value = false;
+	};
+
+	template<typename UiElementType, std::size_t N>
+	AuCarExpErrorCode SetupUIData(std::array<UiElementType, N> elements, 
+		                          AuCarExpArray<typename UiElementType::AUDataType>& data)
+	{
+		if (data.GetCount() != elements.size())
+			return AuCarExpErrorCode_UnknownError;
+
+		int idx = 0;
+		for (auto& element : elements)
+		{
+			element.update(data[idx]);
+			idx += 1;
+		}
+		return AuCarExpErrorCode_Success;
+	}
 }
